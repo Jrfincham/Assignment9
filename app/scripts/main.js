46a7fd9f85a5aba7ac627ca8bde196d278659cd9
Jason Robert Fincham <JRFincham@yahoo.com>
2015-01-21 16:57:52 -0500

    Finding img tags

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,11 @@
-var items
+var containermain = $('.maincontent'),
+  img_url,
+  img_tag;
 
-var items = 
+items.results.forEach( function(item){
+
+  img_url = item.Images[0].url_fullxfull;
+  img_tag = itm src ='" + img_url + "'>;
+});
+
+ // $('maincontent').append(img src="' + item.images[0].url_fullxfull +'">');
