1b75da503bd170a801665d4647455ea8925b7ed2
Jason Robert Fincham <JRFincham@yahoo.com>
2015-01-22 09:03:15 -0500

    started styling maincontent

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,15 +3,16 @@ items.results.forEach( function(item){
   var title = item.title;
   var price = item.price;
   var currencycode = item.currency_code;
-  var userid = item.user_id;
+  var userid = item.shop_name;
 
   img_url = item.Images[0].url_170x135;
   var listDiv = $('<div></div>');
+  $(listDiv).addClass('icons');
   $(listDiv).append($('<img/>').prop('src', img_url));
   $(listDiv).append($('<span></span>').text(title).prop('id', 'titleSpan'));
-  $(listDiv).append($('<span></span>').text(userid).prop());
-  $(listDiv).append($('<span></span>').text(price));
-  $(listDiv).append($('<span></span>').text(currencycode));
-  $(listDiv).addClass('icons');
+  $(listDiv).append($('<span></span>').text(userid).prop('id', 'shopnameSpan'));
+  $(listDiv).append($('<span></span>').text(price).prop('id', 'priceSpan'));
+  $(listDiv).append($('<span></span>').text(currencycode).prop('id', 'currencycodeSpan'));
+
   $('.maincontent').append(listDiv);
 });
