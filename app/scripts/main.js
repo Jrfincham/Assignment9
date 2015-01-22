c67f1f05c00c8e0d6fc13f8ce3f612f43f3d0ac7
Jason Robert Fincham <JRFincham@yahoo.com>
2015-01-21 23:32:22 -0500

    Maincontent divs on page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,11 +1,17 @@
-var containermain = $('.maincontent'),
-  img_url,
-  img_tag;
 
 items.results.forEach( function(item){
+  var title = item.title;
+  var price = item.price;
+  var currencycode = item.currency_code;
+  var userid = item.user_id;
 
-  img_url = item.Images[0].url_fullxfull;
-  img_tag = itm src ='" + img_url + "'>;
+  img_url = item.Images[0].url_170x135;
+  var listDiv = $('<div></div>');
+  $(listDiv).append($('<img/>').prop('src', img_url));
+  $(listDiv).append($('<span></span>').text(title).prop('id', 'titleSpan'));
+  $(listDiv).append($('<span></span>').text(userid).prop());
+  $(listDiv).append($('<span></span>').text(price));
+  $(listDiv).append($('<span></span>').text(currencycode));
+  $(listDiv).addClass('icons');
+  $('.maincontent').append(listDiv);
 });
-
- // $('maincontent').append(img src="' + item.images[0].url_fullxfull +'">');
